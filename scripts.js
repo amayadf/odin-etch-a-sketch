4773ead3172cf27dfdb1a814d603cb20381a2178
amayadf <106173666+amayadf@users.noreply.github.com>
2023-11-21 13:01:59 -0500

    Implement first function to create and append squares

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 let squareAmount = 16;
 
 
+function createSquares(amount) {
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+    }
+}
+
+//event listeners
+let grid = document.querySelector('.grid');
+
