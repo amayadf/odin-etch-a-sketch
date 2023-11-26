c868a4031b9756fa36e381e9bed4943f96b54a35
amayadf <106173666+amayadf@users.noreply.github.com>
2023-11-26 12:58:33 -0500

    Finish HTML markup

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-let squareAmount = 16;
-
-
-function createSquares(amount) {
-    for(let i = 0; i < amount; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
-    }
-}
-
-//event listeners
-let grid = document.querySelector('.grid');
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'color';
 
