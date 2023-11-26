ce8de62944156ab6504b6a6f27e87c2db812ecd9
amayadf <106173666+amayadf@users.noreply.github.com>
2023-11-26 16:42:35 -0500

    Implement major functions and event listeners

--- scripts.js
+++ scripts.js
@@ -1,3 +1,93 @@
-const DEFAULT_SIZE = 16;
+const DEFAULT_SIZE = 4;
 const DEFAULT_MODE = 'color';
+const DEFAULT_COLOR = '#ffffff';
 
+let size = DEFAULT_SIZE;
+let mode = DEFAULT_MODE;
+let color = DEFAULT_COLOR;
+
+//function to create grid
+function updateSize(newSize) {
+    size = newSize;
+}
+
+function createGrid(){
+    pixelAmount = size * size;
+    for(let i = 0; i < pixelAmount; i++) {
+        let gridPixel = document.createElement('div');
+        grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        grid.insertAdjacentElement('beforeend', gridPixel);
+    }
+    let gridPixels = document.querySelectorAll('.grid div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('click', colorPixel));
+}
+
+//function to change mode
+function changeMode(event) {
+    //mode = event.target.id;
+    switch(event.target.id) {
+        case 'color':
+            mode = 'color';
+            break;
+        case 'rainbow':
+            mode = 'rainbow';
+            break;
+        case 'monochrome':
+            mode = 'monochrome';
+            break;
+        case 'eraser':
+            mode = 'eraser';
+            break;
+    }
+}
+
+//function to color pixel
+function colorPixel() {
+    switch(mode){
+        case 'color':
+            this.style.backgroundColor = colorPicker.value;
+            break;
+        case 'rainbow':
+            this.style.backgroundColor = generateRandomRGB();
+            break;
+        case 'monochrome':
+            this.style.backgroundColor = '#000000';
+            break;
+        case 'eraser':
+            this.style.backgroundColor = DEFAULT_COLOR;
+            break;
+    }
+}
+
+//function to clear grid
+function clearGrid() {
+    let gridPixels = document.querySelectorAll('.grid div');
+    gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = '#ffffff')
+}
+
+//obtaining elements from the document
+let grid = document.querySelector('.grid');
+let colorButtons = document.querySelectorAll('.color-options button');
+let clearButton = document.querySelector('.grid-options button');
+let slider = document.querySelector('.slider');
+let colorPicker = document.querySelector('button input');
+
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', changeMode));
+clearButton.addEventListener('click', clearGrid);
+slider.addEventListener('input', (e) => {
+    updateSize(e.target.value);
+    createGrid();
+})
+colorPicker.addEventListener('input', (e) => color = e.target.value);
+
+//create grid with the default size when the site loads
+createGrid();
+
+//helper functions
+generateRandomRGB() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    return `rgb(${x}, ${y}, ${z})`;  
+}
